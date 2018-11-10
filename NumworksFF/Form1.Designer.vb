b17d284e8c31e9a823e82dfb52fbc76490ea8b75
Turion 64 Guy <thecrafteur88489@gmail.com>
2018-11-10 12:17:50 +0100

    Initial Beta 0.1
    Initial Beta 0.1

---
+++ NumworksFF/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.flash_button = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.StaTextBox = New System.Windows.Forms.TextBox()
+        Me.Process1 = New System.Diagnostics.Process()
+        Me.OpenFileDialogBin = New System.Windows.Forms.OpenFileDialog()
+        Me.SelectFile = New System.Windows.Forms.Button()
+        Me.labelName = New System.Windows.Forms.Label()
+        Me.Clabel = New System.Windows.Forms.Label()
+        Me.DfuUtilTextBox = New System.Windows.Forms.TextBox()
+        Me.CheckDevice = New System.Windows.Forms.Button()
+        Me.UserMsgLabel = New System.Windows.Forms.Label()
+        Me.CalcStateLabel = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ClearTemp = New System.Windows.Forms.CheckBox()
+        Me.FwNameLabel = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'flash_button
+        '
+        Me.flash_button.Location = New System.Drawing.Point(180, 130)
+        Me.flash_button.Name = "flash_button"
+        Me.flash_button.Size = New System.Drawing.Size(135, 23)
+        Me.flash_button.TabIndex = 2
+        Me.flash_button.Text = "Flasher"
+        Me.flash_button.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 360)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'StaTextBox
+        '
+        Me.StaTextBox.Location = New System.Drawing.Point(12, 159)
+        Me.StaTextBox.Multiline = True
+        Me.StaTextBox.Name = "StaTextBox"
+        Me.StaTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.StaTextBox.ShortcutsEnabled = False
+        Me.StaTextBox.Size = New System.Drawing.Size(470, 134)
+        Me.StaTextBox.TabIndex = 2
+        '
+        'Process1
+        '
+        Me.Process1.StartInfo.Domain = ""
+        Me.Process1.StartInfo.LoadUserProfile = False
+        Me.Process1.StartInfo.Password = Nothing
+        Me.Process1.StartInfo.StandardErrorEncoding = Nothing
+        Me.Process1.StartInfo.StandardOutputEncoding = Nothing
+        Me.Process1.StartInfo.UserName = ""
+        Me.Process1.SynchronizingObject = Me
+        '
+        'OpenFileDialogBin
+        '
+        Me.OpenFileDialogBin.FileName = "OpenFileDialog1"
+        Me.OpenFileDialogBin.Title = "Ouvrir un fichier binaire à flasher"
+        '
+        'SelectFile
+        '
+        Me.SelectFile.Location = New System.Drawing.Point(12, 130)
+        Me.SelectFile.Name = "SelectFile"
+        Me.SelectFile.Size = New System.Drawing.Size(135, 23)
+        Me.SelectFile.TabIndex = 1
+        Me.SelectFile.Text = "Ouvrir un firmware"
+        Me.SelectFile.UseVisualStyleBackColor = True
+        '
+        'labelName
+        '
+        Me.labelName.AutoSize = True
+        Me.labelName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelName.Location = New System.Drawing.Point(95, 9)
+        Me.labelName.Name = "labelName"
+        Me.labelName.Size = New System.Drawing.Size(252, 24)
+        Me.labelName.TabIndex = 4
+        Me.labelName.Text = "Numworks Firmware Flasher"
+        '
+        'Clabel
+        '
+        Me.Clabel.AutoSize = True
+        Me.Clabel.Location = New System.Drawing.Point(12, 337)
+        Me.Clabel.Name = "Clabel"
+        Me.Clabel.Size = New System.Drawing.Size(113, 13)
+        Me.Clabel.TabIndex = 4
+        Me.Clabel.Text = "v__ par Turion 64 Guy"
+        '
+        'DfuUtilTextBox
+        '
+        Me.DfuUtilTextBox.Location = New System.Drawing.Point(12, 299)
+        Me.DfuUtilTextBox.Multiline = True
+        Me.DfuUtilTextBox.Name = "DfuUtilTextBox"
+        Me.DfuUtilTextBox.ReadOnly = True
+        Me.DfuUtilTextBox.ShortcutsEnabled = False
+        Me.DfuUtilTextBox.Size = New System.Drawing.Size(470, 30)
+        Me.DfuUtilTextBox.TabIndex = 6
+        '
+        'CheckDevice
+        '
+        Me.CheckDevice.Location = New System.Drawing.Point(347, 130)
+        Me.CheckDevice.Name = "CheckDevice"
+        Me.CheckDevice.Size = New System.Drawing.Size(135, 23)
+        Me.CheckDevice.TabIndex = 3
+        Me.CheckDevice.Text = "Vérifier état calculatrice"
+        Me.CheckDevice.UseVisualStyleBackColor = True
+        '
+        'UserMsgLabel
+        '
+        Me.UserMsgLabel.AutoSize = True
+        Me.UserMsgLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UserMsgLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.UserMsgLabel.Location = New System.Drawing.Point(13, 69)
+        Me.UserMsgLabel.Name = "UserMsgLabel"
+        Me.UserMsgLabel.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.UserMsgLabel.Size = New System.Drawing.Size(0, 24)
+        Me.UserMsgLabel.TabIndex = 8
+        '
+        'CalcStateLabel
+        '
+        Me.CalcStateLabel.AutoSize = True
+        Me.CalcStateLabel.Location = New System.Drawing.Point(14, 114)
+        Me.CalcStateLabel.Name = "CalcStateLabel"
+        Me.CalcStateLabel.Size = New System.Drawing.Size(88, 13)
+        Me.CalcStateLabel.TabIndex = 9
+        Me.CalcStateLabel.Text = "état calculatrice :"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.NumworksFF.My.Resources.Resources.numworksfirmwareflasher_logo1
+        Me.PictureBox1.InitialImage = Global.NumworksFF.My.Resources.Resources.numworksfirmwareflasher_logo1
+        Me.PictureBox1.Location = New System.Drawing.Point(412, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(70, 69)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'ClearTemp
+        '
+        Me.ClearTemp.AutoSize = True
+        Me.ClearTemp.Location = New System.Drawing.Point(268, 335)
+        Me.ClearTemp.Name = "ClearTemp"
+        Me.ClearTemp.Size = New System.Drawing.Size(214, 17)
+        Me.ClearTemp.TabIndex = 6
+        Me.ClearTemp.Text = "Supprimer dossier temporaire en quittant"
+        Me.ClearTemp.UseVisualStyleBackColor = True
+        '
+        'FwNameLabel
+        '
+        Me.FwNameLabel.AutoSize = True
+        Me.FwNameLabel.Location = New System.Drawing.Point(14, 80)
+        Me.FwNameLabel.Name = "FwNameLabel"
+        Me.FwNameLabel.Size = New System.Drawing.Size(0, 13)
+        Me.FwNameLabel.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 357)
+        Me.Controls.Add(Me.FwNameLabel)
+        Me.Controls.Add(Me.ClearTemp)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.CalcStateLabel)
+        Me.Controls.Add(Me.UserMsgLabel)
+        Me.Controls.Add(Me.CheckDevice)
+        Me.Controls.Add(Me.DfuUtilTextBox)
+        Me.Controls.Add(Me.Clabel)
+        Me.Controls.Add(Me.labelName)
+        Me.Controls.Add(Me.SelectFile)
+        Me.Controls.Add(Me.StaTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.flash_button)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Numwoks Firmware Flasher"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents flash_button As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents StaTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Process1 As System.Diagnostics.Process
+    Friend WithEvents OpenFileDialogBin As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SelectFile As System.Windows.Forms.Button
+    Friend WithEvents labelName As System.Windows.Forms.Label
+    Friend WithEvents Clabel As System.Windows.Forms.Label
+    Friend WithEvents DfuUtilTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents CheckDevice As System.Windows.Forms.Button
+    Friend WithEvents UserMsgLabel As System.Windows.Forms.Label
+    Friend WithEvents CalcStateLabel As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ClearTemp As System.Windows.Forms.CheckBox
+    Friend WithEvents FwNameLabel As System.Windows.Forms.Label
+
+End Class
---
+++ NumworksFF/Form1.vb
@@ -0,0 +1,204 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim DfuutilExe As String
+    Dim NFFver As String = "0.1b"
+    Dim UserBinFile As String
+    Dim TempPath As String
+    Dim BinPath As String = "0"
+    Dim DevicePresent As Boolean = False
+    Dim DeviceMode As String
+    Dim Aborted As Boolean
+
+    Sub StatusWrite(ByVal text As String)
+        StaTextBox.AppendText(text & vbNewLine)
+    End Sub
+    Sub StatusWriteLn(ByVal text As String)
+        StaTextBox.AppendText(text)
+    End Sub
+    Sub ErrorWrite(ByVal text As String)
+        StaTextBox.AppendText("-----------[ERR/DFU]---------" & vbNewLine & text & "---------------------------------------" & vbNewLine)
+    End Sub
+    Public Function ExecDfuutil()
+        Process1.StartInfo.FileName = DfuutilExe
+        Process1.StartInfo.WorkingDirectory = TempPath
+        Process1.StartInfo.Arguments = "-i 0 -a 0 -s 0x08000000:leave -D firmware.bin"
+        Process1.StartInfo.UseShellExecute = False
+        Process1.StartInfo.RedirectStandardOutput = True
+        Process1.StartInfo.RedirectStandardError = True
+        Process1.StartInfo.CreateNoWindow = False
+        Process1.StartInfo.WindowStyle = ProcessWindowStyle.Maximized
+
+
+        Process1.Start()
+
+        While Process1.Responding
+            DfuUtilTextBox.AppendText(vbNewLine & Process1.StandardOutput.ReadLine)
+            'StatusWrite(Process1.StandardError.ReadLine)
+        End While
+        ErrorWrite(Process1.StandardError.ReadToEnd)
+        Process1.WaitForExit()
+
+        Return 1
+    End Function
+    Public Function TestForDev()
+        Dim output As String
+        Process1.StartInfo.FileName = DfuutilExe
+        Process1.StartInfo.WorkingDirectory = TempPath
+        Process1.StartInfo.Arguments = "-l"
+        Process1.StartInfo.UseShellExecute = False
+        Process1.StartInfo.RedirectStandardOutput = True
+        Process1.StartInfo.RedirectStandardError = True
+        Process1.StartInfo.CreateNoWindow = True
+
+        Process1.Start()
+
+        'ErrorWrite(Process1.StandardError.ReadToEnd)
+        output = Process1.StandardOutput.ReadToEnd
+        DevicePresent = output.Contains("Flash")
+        Label1.Text = DevicePresent
+
+        CalcStateLabel.Text = "Calculatrice non branchée ou reconnue"
+        If DevicePresent Then
+            StatusWrite("Calculatrice branchée")
+        End If
+        If output.Contains("@Flash") Then
+            DeviceMode = "ramdisk"
+            StatusWrite("Calculatrice allumée")
+            CalcStateLabel.Text = "Calculatrice branchée et allumée"
+        ElseIf output.Contains("@Internal Flash") Then
+            StatusWrite("Calculatrice en mode DFU (écran éteint)")
+            CalcStateLabel.Text = "Calculatrice en mode DFU (écran éteint)"
+            DeviceMode = "DFU"
+        End If
+
+        Label1.Text = DeviceMode
+
+        Return DevicePresent
+    End Function
+    Public Function GotoDFU()
+        Dim isok As Boolean = False
+        TestForDev()
+        If DeviceMode = vbNullString Then
+            If MessageBox.Show("Veuillez brancher ou re-brancher la calculatrice au PC", "Info", MessageBoxButtons.OKCancel) = DialogResult.Cancel Then
+                Aborted = True
+            End If
+            isok = False
+        Else
+            Select Case DeviceMode
+                Case "ramdisk"
+                    If MessageBox.Show("Laissez la calculatrice branchée et appuyez sur le bouton RESET à l'arrière de la calculatrice", "Info", MessageBoxButtons.OKCancel) = DialogResult.Cancel Then
+                        Aborted = True
+                    End If
+                    isok = False
+                Case "DFU"
+                    If MessageBox.Show("Une fenêtre noire va apparaitre, veuillez ne pas la fermer, jusqu'a ce que votre calculatrice se rallume", "Info", MessageBoxButtons.OKCancel) = DialogResult.Cancel Then
+                        Aborted = True
+                    End If
+                    isok = True
+            End Select
+        End If
+
+        Return isok
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        StatusWrite("#### Numworks Firmware Flasher v" & NFFver)
+        Clabel.Text = "v" & NFFver & " par Turion 64 Guy // 2018"
+        StaTextBox.ReadOnly = True
+        TempPath = My.Computer.FileSystem.SpecialDirectories.Temp & "\NumworksFF\"
+        DfuutilExe = TempPath & "dfu-util.exe"
+        OpenFileDialogBin.Filter = "Fichier Firware (.bin) |*.bin"
+        Me.TopMost = True
+        If System.Environment.Is64BitOperatingSystem Then
+            StatusWrite("Windows 64bits")
+        Else
+            StatusWrite("Windows 32bits")
+        End If
+
+        If Not System.IO.Directory.Exists(TempPath) Then
+            System.IO.Directory.CreateDirectory(TempPath)
+            StatusWrite("Dossier temporaire créé")
+        End If
+
+        StatusWrite("Dossier temporaire : " & TempPath)
+        Label1.Text = DfuutilExe
+
+        Using sCreateMSIFile As New FileStream(DfuutilExe, FileMode.Create)
+            sCreateMSIFile.Write(My.Resources.dfu_util, 0, My.Resources.dfu_util.Length)
+            StatusWrite("dfu-util.exe copié")
+        End Using
+        TestForDev()
+
+    End Sub
+
+    Private Sub flash_button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles flash_button.Click
+        If UserBinFile = "OpenFileDialog1" Or UserBinFile = vbNullString Then
+            MessageBox.Show("Vous devez choisir un fichier")
+        Else
+            While Not GotoDFU()
+                If Aborted Then
+                    Exit While
+                End If
+            End While
+            If Not Aborted Then
+                ExecDfuutil()
+                StatusWrite("Démarrage du flash, veuillez patienter")
+            End If
+            Aborted = False
+        End If
+        'StatusWrite("loli")
+        'StatusWrite("loli2")
+    End Sub
+
+    Private Sub SelectFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SelectFile.Click
+        OpenFileDialogBin.Reset()
+        OpenFileDialogBin.Filter = "Fichier Firmware (.bin) |*.bin"
+        'UserBinFile = "OpenFileDialog1"
+        OpenFileDialogBin.ShowDialog()
+        UserBinFile = OpenFileDialogBin.FileName
+        BinPath = TempPath & "firmware.bin"
+        Label1.Text = UserBinFile
+        If UserBinFile = "OpenFileDialog1" Or UserBinFile = vbNullString Then
+            MessageBox.Show("Vous devez choisir un fichier")
+            FwNameLabel.Text = "Firmware séléctionné : " & "aucun"
+        Else
+            StatusWrite("Fichier : " & UserBinFile)
+            StatusWriteLn("Copie du firmware dans le dossier temporaire...")
+            If System.IO.File.Exists(BinPath) Then
+                System.IO.File.Delete(BinPath)
+                'StatusWriteLn("suppression...")
+            End If
+            System.IO.File.Copy(UserBinFile, BinPath)
+            If System.IO.File.Exists(BinPath) Then
+                StatusWrite("copié !")
+                FwNameLabel.Text = "Firmware séléctionné : " & UserBinFile
+            End If
+        End If
+    End Sub
+
+    Private Sub CheckDevice_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckDevice.Click
+        TestForDev()
+        If DevicePresent Then
+
+        End If
+    End Sub
+    Private Sub Form1_Closing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        BinPath = TempPath & "firmware.bin"
+        If System.IO.File.Exists(BinPath) Then
+            System.IO.File.Delete(BinPath)
+        End If
+        If ClearTemp.Checked Then
+            If System.IO.File.Exists(DfuutilExe) Then
+                System.IO.File.Delete(DfuutilExe)
+            End If
+            If System.IO.Directory.Exists(TempPath) Then
+                System.IO.Directory.Delete(TempPath)
+            End If
+        End If
+    End Sub
+
+    Private Sub Clabel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Clabel.Click
+        MessageBox.Show("Version " & NFFver & vbNewLine & "VB Master Race :)" & vbNewLine & "Utilisant le logiciel libre dfu-util (v0.9)")
+    End Sub
+
+End Class
